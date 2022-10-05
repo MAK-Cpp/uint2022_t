4d5982a54275fc523a9b46f33dc3a3b92c4fe5fd
Maksim <spartmenik@gmail.com>
2022-10-05 10:58:16 +0300

    realized in bin, but without decimal cout

--- lib/number.cpp
+++ lib/number.cpp
@@ -1,8 +1,7 @@
 #include "number.h"
 
-const uint32_t KBaseOfNumericSystem = 1e9;
+const uint8_t kBitByID[8] = {0b10000000, 0b01000000, 0b00100000, 0b00010000, 0b00001000, 0b00000100, 0b00000010, 0b00000001};
 
-const uint2022_t KMaxOfStruct = {{481560, 916771158, 684800786, 922703235, 625631274, 322714142, 263414417, 884163925, 873322306, 437689024, 231009526, 751394401, 758326916, 367106052, 34484602, 375642882, 110959089, 521812209, 947069992, 139877256, 8949136, 579813164, 413834190, 131240610, 432508865, 633901300, 457687591, 589632190, 325582710, 683886781, 973951695, 733384278, 544896131, 740867054, 246692573, 31629150, 247882082, 682647773, 168904426, 336814855, 367810693, 467547461, 780797071, 163567159, 452928068, 892906992, 787178135, 839959347, 223507647, 240845924, 670958716, 173279750, 751341651, 541295792, 537288393, 481542519, 773223140, 547524361, 834615428, 274169543, 954961376, 881442030, 303829940, 191406452, 725012875, 774576546, 969913778, 507874304}, 0};
 
 void errorExit(int error = -1) {
     switch (error) {
@@ -14,47 +13,101 @@ void errorExit(int error = -1) {
     }
 }
 
+// void addToBit(uint2022_t& value, const uint16_t& bit, uint16_t add) {
+//     uint16_t razryad = 0;
+//     for (uint16_t i = bit; i < 253 * 8 && (add > 0 || razryad > 0); i++) {
+//         if ((add & 1) == 0) {
+//             if (razryad == 1) {
+//                 if ((value.big_uint[i / 8] & kBitByID[(i & 7)]) == 0) {
+//                     // 0 + 1 + 0 = 1, razryad = 0
+//                     value.big_uint[i / 8] += kBitByID[(i & 7)];
+//                     razryad = 0;
+//                 } else {
+//                     // 0 + 1 + 1 = 0, razryad = 1
+//                     value.big_uint[i / 8] -= kBitByID[(i & 7)];
+//                 }
+//             }
+//             // 0 + 0 + x = x, razryad = 0
+//         } else { // (add & 1) == 1
+//             if (razryad == 0) {
+//                 if ((value.big_uint[i / 8] & kBitByID[(i & 7)]) == 0) {
+//                     // 1 + 0 + 0 = 1, razryad = 0
+//                     value.big_uint[i / 8] += kBitByID[(i & 7)];
+//                 } else {
+//                     // 1 + 0 + 1 = 0, razryad = 1
+//                     value.big_uint[i / 8] -= kBitByID[(i & 7)];
+//                     razryad = 1;
+//                 }
+//             }
+//             // 1 + 1 + x = x, razryad = 1
+//         }
+//         add /= 2;
+//         if ((value.big_uint[i / 8] & kBitByID[(i & 7)]) == kBitByID[(i & 7)] && i > value.first_non_zero_bit) {
+//             value.first_non_zero_bit = i;
+//         }
+//     }
+//     if (add > 0 || razryad > 0) {
+//         std::cout << "addToBit\n";
+//         errorExit();
+//     }
+// }
+
+uint8_t reverseBlock(const uint8_t& block) {
+    uint8_t ans = 0;
+    for (uint8_t i = 0; i < 8; ++i) {
+        if ((block & kBitByID[i]) == kBitByID[i]) {
+            ans += kBitByID[7 - i];
+        } 
+    }
+    return ans;
+}
+
 uint2022_t from_uint(uint32_t i) {
     uint2022_t ans;
-    ans.big_uint[ans.start] = i;
+    if (i == 0) {
+        return ans;
+    }
+    for (;i > 0; i = (i >> 1), ans.first_non_zero_bit++) {
+        if (i & 1 == 1) {
+            ans.big_uint[ans.first_non_zero_bit / 8] += kBitByID[ans.first_non_zero_bit & 7];
+        }
+    }
+    ans.first_non_zero_bit--;
+
     return ans;
 }
 
 uint2022_t from_string(const char* buff) {
-    // один блок - 9 цифр -> buff / 9 с округлением вверх =
-    // = кол-ву блоков, если buff % 9 == 0 все легко, иначе
-    // первый блок - [0; buff % 9 - 1], остальные так же как 
-    // до этого
     uint2022_t ans;
-    std::cout << ans.big_uint[ans.start] << std::endl;
-    uint32_t len = 0;
-    for (uint32_t count = 0; buff[count] != '\0'; ++count) {
-        len++;
-    }
-    if (len > ans.end * 9) {
-        errorExit();
-    }
-    uint32_t remainder = len % 9;
-    ans.start = ans.end - (len + 9 - 1) / 9;
-
-    std::cout << len << ' ' << ans.start << std::endl;
-
-    for (uint32_t i = 0, block_id = ans.start, i_remainder = 0; buff[i] != '\0'; ++i, i_remainder++) {
-        std::cout << i << ' ' << buff[i] << ' ' << block_id << std::endl;
-        if (i_remainder == 9) {
-            i_remainder = 0;
-        }
-        if (i_remainder == remainder) {
-            block_id++;
-        }
-        ans.big_uint[block_id] = ans.big_uint[block_id] * 10 + (buff[i] - '0');
-        std::cout << ans.big_uint[block_id] << std::endl;
+    const uint2022_t step = from_uint(10);
+    for (uint16_t i = 0; buff[i] != '\0'; ++i) {
+        uint2022_t add_number = from_uint(buff[i] - '0');
+        ans = ans * step + add_number;
+        // std::cout << ans << ent;
     }
     return ans;
 }
 
 uint2022_t operator+(const uint2022_t& lhs, const uint2022_t& rhs) {
-    return uint2022_t();
+    uint2022_t ans;
+    uint16_t maximum_bit = std::max(lhs.first_non_zero_bit, rhs.first_non_zero_bit);
+    uint16_t razryad = 0;
+    for (uint16_t ans_block_id = 0; ans_block_id <= (maximum_bit + 8 - 1) / 8; ans_block_id++) {
+        uint16_t result = reverseBlock(lhs.big_uint[ans_block_id]) + reverseBlock(rhs.big_uint[ans_block_id]) + razryad + reverseBlock(ans.big_uint[ans_block_id]);
+        ans.big_uint[ans_block_id] = reverseBlock(result & 255);
+        razryad = result / 256;
+    }
+    if (razryad != 0) {
+        ans.big_uint[maximum_bit / 8 + 1] = reverseBlock(razryad);
+    }
+    ans.first_non_zero_bit = ans.big_uint.size() * 8 - 1;
+    while (ans.big_uint[ans.first_non_zero_bit / 8] == 0 && ans.first_non_zero_bit > 7) {
+        ans.first_non_zero_bit -= 8;
+    }
+    while ((ans.big_uint[ans.first_non_zero_bit / 8] & kBitByID[(ans.first_non_zero_bit & 7)]) == 0 && ans.first_non_zero_bit > 0) {
+        ans.first_non_zero_bit--;
+    }
+    return ans;
 }
 
 uint2022_t operator-(const uint2022_t& lhs, const uint2022_t& rhs) {
@@ -62,11 +115,57 @@ uint2022_t operator-(const uint2022_t& lhs, const uint2022_t& rhs) {
 }
 
 uint2022_t operator*(const uint2022_t& lhs, const uint2022_t& rhs) {
-    // перемножение будет больше чем uint64_t, возможно переделать под uint32_t, 
-    // чтобы результат был uint_64t
-
-
-    return uint2022_t();
+    uint2022_t ans;
+    if (lhs.first_non_zero_bit + rhs.first_non_zero_bit >= 2022) {
+        std::cout << "OPERATOR * ERROR\n";
+        // std::cout << lhs.first_non_zero_bit _ rhs.first_non_zero_bit << ent;
+        errorExit();
+    }
+    for (uint16_t lhs_block_id = 0; lhs_block_id <= lhs.first_non_zero_bit / 8; ++lhs_block_id) {
+        // std::printf("check 3\n");
+        uint8_t razryad = 0;
+        for (uint16_t rhs_block_id = 0; rhs_block_id <= rhs.first_non_zero_bit / 8; ++rhs_block_id) {
+            uint16_t rezult = reverseBlock(lhs.big_uint[lhs_block_id]) * reverseBlock(rhs.big_uint[rhs_block_id]) + razryad + reverseBlock(ans.big_uint[lhs_block_id + rhs_block_id]);
+            ans.big_uint[lhs_block_id + rhs_block_id] = reverseBlock((rezult & 255));
+            razryad = rezult / 256;
+            // std::cout << "operator*\n";
+            // std::cout << lhs _ rhs << ent;
+            // std::printf("lhs_block_id = %u, rhs_block_id = %u, rezult = %u, ans.big_uint[lhs_block_id + rhs_block_id] = %u, razryad = %u \n", lhs_block_id, rhs_block_id, rezult, ans.big_uint[lhs_block_id + rhs_block_id], razryad);
+            // std::cout << lhs_block_id _ rhs_blocsk_id _ rezult _ (uint16_t)ans.big_uint[lhs_block_id + rhs_block_id] _ (uint16_t)razryad << ent;
+        }
+        // std::printf("check 1\n");
+        if (razryad != 0) {
+            if (lhs_block_id + rhs.first_non_zero_bit / 8 + 1 >= 2022) {
+                errorExit();
+            }
+            ans.big_uint[lhs_block_id + rhs.first_non_zero_bit / 8 + 1] = reverseBlock(razryad);
+        } 
+        // std::printf("check 2\n");
+    }
+    ans.first_non_zero_bit = ans.big_uint.size() * 8 - 1;
+    // std::printf("first non zero bit in ans = %u \n", ans.first_non_zero_bit);
+    while (ans.big_uint[ans.first_non_zero_bit / 8] == 0 && ans.first_non_zero_bit > 7) {
+        // std::printf("ans.big_uint[first non zero bit / 8] = %u, first non zero bit = %u \n", ans.big_uint[ans.first_non_zero_bit / 8], ans.first_non_zero_bit);
+        ans.first_non_zero_bit -= 8;
+    }
+    while ((ans.big_uint[ans.first_non_zero_bit / 8] & kBitByID[(ans.first_non_zero_bit & 7)]) == 0 && ans.first_non_zero_bit > 0) {
+        ans.first_non_zero_bit--;
+    }
+    /*
+    uint16_t maximum_bit = lhs.first_non_zero_bit + rhs.first_non_zero_bit;
+    for (uint16_t ans_bit_id = 0; ans_bit_id <= maximum_bit; ans_bit_id++) {
+        // bit_id - степень двойки, поэтому начинается с нуля
+        uint16_t count_to_add = 0;
+        for (uint16_t lhs_bit_id = ans_bit_id, rhs_bit_id = 0; rhs_bit_id <= ans_bit_id; lhs_bit_id--, rhs_bit_id++) {
+            if ((lhs.big_uint[lhs_bit_id / 8] & kBitByID[(lhs_bit_id & 7)]) == kBitByID[(lhs_bit_id & 7)] && 
+                (rhs.big_uint[rhs_bit_id / 8] & kBitByID[(rhs_bit_id & 7)]) == kBitByID[(rhs_bit_id & 7)]) {
+                count_to_add++;
+            }
+        }
+        addToBit(ans, ans_bit_id, count_to_add);
+    }
+    */
+    return ans;
 }
 
 uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs) {
@@ -81,20 +180,29 @@ bool operator!=(const uint2022_t& lhs, const uint2022_t& rhs) {
     return false;
 }
 
-std::ostream& operator<<(std::ostream& stream, const uint2022_t& value) {
-    for (uint8_t i = value.start; i < value.end; ++i) {
-        if (i != value.start) {
-            if (value.big_uint[i] >= 1e8) {
-                stream << value.big_uint[i];
-            } else {
-                for (int i = log10(value.big_uint[i]) + 1; i < 9; ++i) {
-                    stream << 0;
-                }
-                stream << value.big_uint[i];
-            }
-        } else {
-            stream << value.big_uint[i];
+bool operator>(const uint2022_t& lhs, const uint2022_t& rhs) {
+    if (lhs.first_non_zero_bit != rhs.first_non_zero_bit) {
+        return lhs.first_non_zero_bit > rhs.first_non_zero_bit;
+    }
+    for (int16_t bit_id = lhs.first_non_zero_bit; bit_id >= 0; bit_id--) {
+        if ((lhs.big_uint[bit_id / 8] & kBitByID[(bit_id & 7)]) != (rhs.big_uint[bit_id / 8] & kBitByID[(bit_id & 7)])) {
+            return (lhs.big_uint[bit_id / 8] & kBitByID[(bit_id & 7)]) > (rhs.big_uint[bit_id / 8] & kBitByID[(bit_id & 7)]);
         }
     }
+    return false;
+}
+
+std::ostream& operator<<(std::ostream& stream, const uint2022_t& value) {
+    // вывод в двоичном формате, переделать
+    
+    const bool is_in_binary = true;
+    if (is_in_binary) {
+        // stream << "\nTEST OUT IN BINARY BASE, REMAKE\n";
+
+        for (int16_t i = value.first_non_zero_bit; i >= 0; i--) {
+            stream << ((value.big_uint[i / 8] & kBitByID[(i & 7)]) == kBitByID[(i & 7)]);
+        }
+    }
+
     return stream;
 }
--- lib/number.h
+++ lib/number.h
@@ -2,17 +2,50 @@
 #include <cinttypes>
 #include <iostream>
 #include <cmath>
+#include <vector> 
 
+#define _ << ' ' <<
+#define ent '\n'
 
 struct uint2022_t {
-    uint32_t big_uint[68] = {};
+    // будем хранить число справа налево, т.е. чем больше
+    // block_id, тем больше число
+    std::vector<uint8_t> big_uint;
 
-    // каждое число от 0 до 999999999, 9 цифр кодирует
-    // 0 <= big_uint[i] < 1e9
+    uint16_t len_in_decimal;
+    uint16_t first_non_zero_bit;
 
-    uint32_t start = 67;
-    const uint32_t end = 68;
+    uint2022_t() {
+        big_uint.resize(253, 0);
+        len_in_decimal = 1;
+        first_non_zero_bit = 0;
+    }
+
+    uint16_t getCountOfBlocks() {
+        return first_non_zero_bit / 8;
+    }
+    /*
+    253 блока по 8 бит = 2024 бит -> 0 <= first_non_zero_bit <= 2023
     
+    каждый блок - число в 8 бит, т.е. с каждым битом можно работать так:
+    I. Узнать, что стоит на k-том бите одного блока
+        1) = big_uint[i] & kBitByID[0];     // 128 = 0b10000000
+        2) = big_uint[i] & kBitByID[1];     //  64 = 0b01000000
+        3) = big_uint[i] & kBitByID[2];     //  32 = 0b00100000
+        4) = big_uint[i] & kBitByID[3];     //  16 = 0b00010000
+        5) = big_uint[i] & kBitByID[4];     //   8 = 0b00001000
+        6) = big_uint[i] & kBitByID[5];     //   4 = 0b00000100
+        7) = big_uint[i] & kBitByID[6];     //   2 = 0b00000010
+        8) = big_uint[i] & kBitByID[7];     //   1 = 0b00000001
+
+    II. Узнать k-ый бит всего числа:
+        1) узнаем блок, в котором этот бит:
+        block = big_uint[k / 8];
+        2) узнаем номер бита:
+        bit_id = (k & 7); 
+        3) узнаем значение бита месте по пункту I:
+        = block & kBitByID[bit_id];
+    */
 };
 
 static_assert(sizeof(uint2022_t) <= 300, "Size of uint2022_t must be no higher than 300 bytes");
@@ -32,5 +65,7 @@ uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs);
 bool operator==(const uint2022_t& lhs, const uint2022_t& rhs);
 
 bool operator!=(const uint2022_t& lhs, const uint2022_t& rhs);
+
+bool operator>(const uint2022_t& lhs, const uint2022_t& rhs);
 
 std::ostream& operator<<(std::ostream& stream, const uint2022_t& value);
